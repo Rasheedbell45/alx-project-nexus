e4a790b22441b1b61c8bc0c9a855f139d2b97e2f
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-10 21:01:19 +0100

    Update index.tsx

--- ecommerce-catalog/src/index.tsx
+++ ecommerce-catalog/src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store";
+import { ApolloProvider } from "@apollo/client";
+import client from "./graphql/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ApolloProvider client={client}>
       <App />
-    </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
