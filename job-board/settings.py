3442ae885a58be303378b4f9d4c798c0effc1786
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-11 19:09:23 +0100

    Create settings.py

---
+++ job-board/settings.py
@@ -0,0 +1,17 @@
+INSTALLED_APPS = [
+    # Django defaults...
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # third-party
+    "rest_framework",
+    "django_filters",
+    "drf_yasg",
+
+    # local
+    "listings",
+]
