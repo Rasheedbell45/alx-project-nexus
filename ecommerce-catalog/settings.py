24509cddcc3995aacf41b50f06ab3e0368ed719d
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-11 15:34:07 +0100

    Update settings.py

--- ecommerce-catalog/settings.py
+++ ecommerce-catalog/settings.py
@@ -38,6 +38,8 @@ DATABASES = {
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+REST_FRAMEWORK['DEFAULT_PAGINATION_CLASS'] = 'rest_framework.pagination.CursorPagination'
+REST_FRAMEWORK['PAGE_SIZE'] = 20
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
