aee94351b4a8d946722c7e2694dcb12c9098bdbf
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-11 18:53:17 +0100

    Update mutation.py

--- socialmedia-feed/src/graphql/mutation.py
+++ socialmedia-feed/src/graphql/mutation.py
@@ -3,3 +3,25 @@ mutation {
     token
   }
 }
+createPost(content: "Hello GraphQL world", mediaUrl: "https://...") {
+    post {
+      id
+      content
+      author {
+        username
+      }
+}
+}
+toggleLike(postId: "1") {
+    liked
+    likesCount
+  }
+addComment(postId: "1", content: "Nice post!") {
+    comment {
+      id
+      content
+      author {
+        username
+      }
+}
+}
