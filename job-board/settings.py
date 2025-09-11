85526d9796b5e1ae78e8b48fbc4e7bd5af2049ba
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-11 19:20:29 +0100

    Update settings.py

--- job-board/settings.py
+++ job-board/settings.py
@@ -1,6 +1,8 @@
 # settings.py
 from datetime import timedelta
 
+REST_FRAMEWORK['DEFAULT_PAGINATION_CLASS'] = 'rest_framework.pagination.CursorPagination'
+REST_FRAMEWORK['PAGE_SIZE'] = 20
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
