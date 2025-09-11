ab24c3880fb51e9ca09c87ccaca8ed1cf5a4a497
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-11 15:38:09 +0100

    Update urls.py

--- ecommerce-catalog/urls.py
+++ ecommerce-catalog/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,3 +16,20 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Ecommerce API",
+      default_version='v1',
+      description="Ecommerce API with products & categories",
+      contact=openapi.Contact(email="you@example.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
