99d571235aba30284741b2913cbeeb124f720dbf
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-20 00:57:54 +0100

    Update urls.py

--- poll-app/polls/urls.py
+++ poll-app/polls/urls.py
@@ -1,7 +1,15 @@
 from rest_framework.routers import DefaultRouter
 from .views import PollViewSet
+from django.urls import path
+from .views import PollListCreateView, PollDetailView, OptionCreateView, VoteCreateView
 
 router = DefaultRouter()
 router.register(r'', PollViewSet, basename='poll')
 
 urlpatterns = router.urls
+urlpatterns = [
+    path('', PollListCreateView.as_view(), name='poll-list-create'),
+    path('<int:pk>/', PollDetailView.as_view(), name='poll-detail'),
+    path('<int:poll_id>/options/', OptionCreateView.as_view(), name='option-create'),
+    path('options/<int:option_id>/vote/', VoteCreateView.as_view(), name='vote-create'),
+]
