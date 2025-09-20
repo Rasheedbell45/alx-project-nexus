2c98f1000a2a2c9a2bd1962753700d987c91649a
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-20 15:26:39 +0100

    Update manage.py

--- poll-app/manage.py
+++ poll-app/manage.py
@@ -1,10 +1,19 @@
-from app import create_app
-from app.extensions import db
-from flask_migrate import MigrateCommand
-from flask_script import Manager
+#!/usr/bin/env python
+import os
+import sys
 
-app = create_app()
-manager = Manager(app)
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poll_app.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed "
+            "and available on your PYTHONPATH environment variable? "
+            "Did you forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
