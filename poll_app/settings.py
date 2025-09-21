39bcfcdaa2e8e00e038a5580a57f8c270b5a19f9
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-21 11:13:46 +0100

    Update settings.py

--- poll_app/settings.py
+++ poll_app/settings.py
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "poll-app.urls"
+ROOT_URLCONF = "poll_app.urls"
 
 TEMPLATES = [
     {
@@ -52,7 +52,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "poll-app.wsgi.application"
+WSGI_APPLICATION = "poll_app.wsgi.application"
 
 # Database (Render provides DATABASE_URL env var)
 DATABASES = {
