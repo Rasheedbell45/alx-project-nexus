70d5f0cf8b776d43dc857f21264059b13750d45d
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-21 09:08:33 +0100

    Update extensions.py

--- poll-app/app/extensions.py
+++ poll-app/app/extensions.py
@@ -1,16 +1,1 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
-from flask_restx import Api
-from flask_caching import Cache
 
-db = SQLAlchemy()
-migrate = Migrate()
-jwt = JWTManager()
-api = Api(
-    title="Polling API",
-    version="1.0",
-    description="APIs for polls, voting, and results",
-    doc="/api/docs"  # Swagger UI served here
-)
-cache = Cache()
