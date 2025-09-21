dbcb8a4f11322d00a58239afc7cc42ff5757f965
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-21 11:26:26 +0100

    Update wsgi.py

--- poll_app/wsgi.py
+++ poll_app/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polling_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poll_app.settings")
 application = get_wsgi_application()
