74b3700a811f144dfa49ef74d36c1597ebaf6feb
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-21 15:16:38 +0100

    Update urls.py

--- poll_app/polls/urls.py
+++ poll_app/polls/urls.py
@@ -1,20 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
-from .views import PollListCreateView, PollDetailView, OptionCreateView, VoteCreateView, PollViewSet
+from .views import PollViewSet, OptionCreateView, VoteCreateView
 
 router = DefaultRouter()
-router.register(r'', PollViewSet, basename='poll')
+router.register(r'polls', PollViewSet, basename='poll')
 
-urlpatterns = router.urls
-urlpatterns = [
-    path('', PollListCreateView.as_view(), name='poll-list-create'),
-    path('<int:pk>/', PollDetailView.as_view(), name='poll-detail'),
-    path('<int:poll_id>/options/', OptionCreateView.as_view(), name='option-create'),
+urlpatterns = router.urls  # includes list, create, retrieve, update, delete, vote, results
 
-    # Voting (supports both styles)
-    path('options/<int:option_id>/vote/', VoteCreateView.as_view(), name='vote-create'),
-    path('<int:poll_id>/vote/', VoteCreateView.as_view(), name='poll-vote'),
-
-    # Results
-    path('<int:poll_id>/results/', PollResultsView.as_view(), name='poll-results'),
+urlpatterns += [
+    path('polls/<int:poll_id>/options/', OptionCreateView.as_view({'get': 'list', 'post': 'create'}), name='option-create'),
+    path('options/<int:option_id>/vote/', VoteCreateView.as_view({'post': 'create'}), name='vote-create'),
+    path('polls/<int:poll_id>/vote/', VoteCreateView.as_view({'post': 'create'}), name='poll-vote'),
 ]
