8ea5fa1e53937c22d18659e77584223478cb8c62
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-21 09:31:56 +0100

    Update urls.py

--- poll-app/users/urls.py
+++ poll-app/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .auth import RegisterAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path("register/", RegisterAPIView.as_view(), name="auth-register"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
