fdf802187732df20fcc75834848b6370859395c0
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-22 04:09:07 +0100

    Update App.jsx

--- poll-app/src/App.jsx
+++ poll-app/src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+// Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
+import Register from "./pages/Register"; // added missing import
 import Polls from "./pages/Polls";
 import Results from "./pages/Results";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -16,12 +18,17 @@ function App() {
       <Navbar />
       <div className="min-h-screen">
         <Routes>
+          {/* Public pages */}
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+
+          {/* Authentication pages */}
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/login" element={<Login />} />
+
+          {/* Protected pages */}
           <Route path="/polls" element={<Polls />} />
           <Route path="/results/:id" element={<Results />} />
         </Routes>
